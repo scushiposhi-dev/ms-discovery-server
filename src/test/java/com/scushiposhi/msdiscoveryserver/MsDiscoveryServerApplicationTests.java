0ffacc79e703b5772bbdd56ca72fba566ca56279
scushiposhi <scushiposhi@yahoo.com>
2023-03-16 19:02:38 +0100

    First commit

---
+++ src/test/java/com/scushiposhi/msdiscoveryserver/MsDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scushiposhi.msdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
